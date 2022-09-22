ce8361e73c1875be727dcd73929fa330aa0ee05f
elena tarasova <nafanichka.ev@gmail.com>
2022-09-21 23:10:51 -0400

    Exersize1

---
+++ Exersize1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Write number");
+int number = int.Parse(Console.ReadLine());
+
+int num1 = number % 100;
+int num2 = number % 10;
+int num3 = (num1 - num2) / 10;
+Console.WriteLine(num3);
