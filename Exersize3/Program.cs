831acc84fa3e9d803c9b60896fb3af7269ec1df3
elena tarasova <nafanichka.ev@gmail.com>
2022-09-21 23:18:34 -0400

    Exersize3

---
+++ Exersize3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Write a number");
+int number = int.Parse(Console.ReadLine());
+
+if (number<=5)
+{
+    Console.WriteLine("This day is a weekday");
+}
+if (number>=6)
+{
+    Console.WriteLine("This day is a weekend");
+}
+
+
+// Console.WriteLine("Write a number");
+// int number = int.Parse(Console.ReadLine());
+
+// if (number <= 5 && number > 0)
+//     Console.WriteLine("This day is a weekday");
+
+// else if (number == 7 || number == 6)
+//     Console.WriteLine("This day is a weekend");
+
+// else 
+//     Console.WriteLine("Wrong week day number");
